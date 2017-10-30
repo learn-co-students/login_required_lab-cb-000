70868e64333147f9120e1a1a49a12387a9639b29
Matthew <github email address>
2017-10-30 10:28:03 +0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  get 'secrets' => 'secrets#show'
+  post 'sessions' => 'sessions#create'
+  get 'sessions/new' => 'sesssions#new'
+  delete 'sessions' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
