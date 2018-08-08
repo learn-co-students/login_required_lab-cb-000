6fc5b72a33afdf6138a223fd26ba9531f9df07be
Alex Segers <a.segers.dev@gmail.com>
2018-08-08 20:27:24 +0000

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,4 +3,14 @@ class SecretsController < ActionController::Base
 
   def show
   end
+
+  def current_user
+    session[:name]
+  end
+  
+  private
+
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
 end
