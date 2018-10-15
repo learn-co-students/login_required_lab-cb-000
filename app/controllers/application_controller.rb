b105ac19b919d265f117f681567823a582b685a5
dapawn <dapawn@gmail.com>
2018-10-15 12:37:04 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+    helper_method :current_user
+
+    def current_user
+      session[:name] unless session[:name].nil? || session[:name].empty?
+    end
+
+    def require_login
+      redirect_to controller: 'sessions', action: 'new' unless current_user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get '/hello' => 'sessions#show'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
