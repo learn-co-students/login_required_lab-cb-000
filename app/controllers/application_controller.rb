30ab532dddfa03b3e0bd02c16a9c7f677d8b61af
Felice Forby <feliceforby@gmail.com>
2018-11-13 07:43:31 +0000

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
-
+  before_action :require_login, only: [:welcome]
+  
   def welcome
-    redirect_to login_path unless logged_in?
   end
 
   def current_user
@@ -14,5 +14,11 @@ class ApplicationController < ActionController::Base
   
   def logged_in?
     !!current_user
+  end
+  
+  private
+  
+  def require_login
+    redirect_to login_path unless logged_in?
   end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,6 @@
 class SecretsController < ApplicationController
+  before_action :require_login
+  
   def show
-    redirect_to login_path unless logged_in?
   end
 end
