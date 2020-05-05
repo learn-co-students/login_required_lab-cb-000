9688a529cc1b67d8c04cac49d51ebb136b818f5b
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 20:36:42 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+  
+    before_action :require_login
+
+def show 
+end 
+
+
+
+  private
+  
+  def require_login 
+    redirect_to root_path unless session.include? :name
+  end 
+  
+end 
